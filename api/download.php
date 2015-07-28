7fcb4d4024bdb00f80e80ba9e1ac899f6f9b5ae8
mailzwj <mailzwj@126.com>
2015-07-28 12:39:57 +0800

    修复下载漏洞，避免服务器资源被下载

--- api/download.php
+++ api/download.php
@@ -1,6 +1,11 @@
 <?php
     if (isset($_GET["source"])) {
         $file = $_GET["source"];
+        $rs = preg_match("/^\.\.\/download\/sprite\.(png|css)$/", $file, $matches);
+        if (!$rs) {
+            echo "非法的资源路径，请检查！";
+            exit;
+        }
         $fsize = getimagesize($file);
         header('Content-type: ' . $fsize['mime']);
         header("Content-Disposition: attachment; filename=" . basename($file));
